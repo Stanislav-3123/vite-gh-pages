ae5530d7412f9af898a507c324e735a35d480d8a
Stanislav Pshedzial <pshedzial.stas@gmail.com>
2024-07-21 21:32:27 +0300

    Updates

--- assets/index-BN4HEkuE.js
+++ assets/index-BN4HEkuE.js
@@ -1,4 +1,4 @@
-import{r as H,g as dt}from"./index-Cf5W1-n4.js";var ct={exports:{}},A={exports:{}},L={exports:{}};/*!
+import{r as H,g as dt}from"./index-BfBcDFJa.js";var ct={exports:{}},A={exports:{}},L={exports:{}};/*!
  * getSize v2.0.3
  * measure size of elements
  * MIT license
