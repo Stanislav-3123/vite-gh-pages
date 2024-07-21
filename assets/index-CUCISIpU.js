3c4848296ae0a7f51b05c4d15a609ee8c9f4c5ea
Stanislav Pshedzial <pshedzial.stas@gmail.com>
2024-07-21 22:26:33 +0300

    Updates

--- assets/index-CUCISIpU.js
+++ assets/index-CUCISIpU.js
@@ -1,4 +1,4 @@
-import{r as H,g as dt}from"./index-BfBcDFJa.js";var ct={exports:{}},A={exports:{}},L={exports:{}};/*!
+import{r as H,g as dt}from"./index-C_-WuyIT.js";var ct={exports:{}},A={exports:{}},L={exports:{}};/*!
  * getSize v2.0.3
  * measure size of elements
  * MIT license
